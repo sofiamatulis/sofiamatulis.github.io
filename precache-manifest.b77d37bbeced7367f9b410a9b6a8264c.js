573f30d46796239a097a3682e4720f7212163d7e
sofiamatulis <sosomatulis@hotmail.com>
2019-01-30 21:26:30 -0500

    Updates

---
+++ precache-manifest.b77d37bbeced7367f9b410a9b6a8264c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "be0ba1f309a729843a04",
+    "url": "/static/js/main.be0ba1f3.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "be0ba1f309a729843a04",
+    "url": "/static/css/main.b9836ff7.chunk.css"
+  },
+  {
+    "revision": "a58c4f1be5a696d994ec446dab2e768c",
+    "url": "/index.html"
+  }
+];
